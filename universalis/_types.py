f70906c57fdbd8a0779884a4e4c9b51ccd2582e4
Alex Nørgaard <umbra@abstractumbra.dev>
2025-06-26 00:45:35 +0100

    do over
    Signed-off-by: Alex Nørgaard <umbra@abstractumbra.dev>

--- universalis/_types.py
+++ universalis/_types.py
@@ -1,22 +1,42 @@
+"""Copyright (C) 2021-2024 Katelynn Cadwallader.
+
+This file is part of Kuma Kuma.
+
+Universalis API wrapper is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+Universalis API wrapper is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Universalis API wrapper; see the file COPYING.  If not, write to the Free
+Software Foundation, 51 Franklin Street - Fifth Floor, Boston, MA
+02110-1301, USA.
+"""
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, NotRequired, Optional, Required, TypedDict
 
 if TYPE_CHECKING:
-    from ._enums import DataCenterEnum, ItemQualityEnum, WorldEnum
+    from ._enums import DataCenter, ItemQuality, World
 
 
 class MarketBoardParams(TypedDict, total=False):
-    world_or_dc: DataCenterEnum | WorldEnum
+    world_or_dc: DataCenter | World
     num_listings: int
     num_history_entries: int
-    item_quality: ItemQualityEnum
+    item_quality: ItemQuality
     trim_item_fields: bool
 
 
-class AggregatedFieldsTyped(TypedDict, total=False):
-    """
-    -> Universalis API Aggregated DC/World Json Response fields.\n
+class AggregatedFields(TypedDict, total=False):
+    """Universalis API Aggregated DC/World Json Response fields.
+
     Related to :class:`UniversalisAPIAggregatedKeysTyped` keys.
     """
 
@@ -26,34 +46,34 @@ class AggregatedFieldsTyped(TypedDict, total=False):
     quantity: float
 
 
-class AggregatedKeysTyped(TypedDict, total=False):
-    """
-    -> Universalis API Aggregated DC/World JSON Response Keys.\n
+class AggregatedKeys(TypedDict, total=False):
+    """Universalis API Aggregated DC/World JSON Response Keys.
+
     Related to :class:`UniversalisAPIAggregatedTyped` keys.
     """
 
-    minListing: AggregatedFieldsTyped
-    recentPurchase: AggregatedFieldsTyped
-    averageSalePrice: AggregatedFieldsTyped
-    dailySaleVelocy: AggregatedFieldsTyped
+    minListing: AggregatedFields
+    recentPurchase: AggregatedFields
+    averageSalePrice: AggregatedFields
+    dailySaleVelocy: AggregatedFields
 
 
-class AggregatedTyped(TypedDict, total=False):
-    """
-    -> Universalis API Aggregated DC/World JSON Response.\n
+class Aggregated(TypedDict, total=False):
+    """Universalis API Aggregated DC/World JSON Response.
+
     `./universalis_data/data/universalis_api_aggregated_dc.json`
     `./universalis_data/data/universalis_api_aggregated_world.json`
     """
 
     itemId: int
-    nq: AggregatedKeysTyped
-    hq: AggregatedKeysTyped
-    worldUploadTimes: AggregatedKeysTyped
+    nq: AggregatedKeys
+    hq: AggregatedKeys
+    worldUploadTimes: AggregatedKeys
 
 
-class CurrentKeysTyped(TypedDict, total=False):
-    """
-    -> Univertsalis API Current DC/World JSON Response Keys. \n
+class CurrentKeys(TypedDict, total=False):
+    """Univertsalis API Current DC/World JSON Response Keys.
+
     Related to :class:`UniversalisAPICurrentTyped` keys.
     """
 
@@ -68,7 +88,7 @@ class CurrentKeysTyped(TypedDict, total=False):
     hq: bool
     isCrafted: bool
     listingID: str
-    materia: list
+    materia: list[...]  # ????
     onMannequin: bool
     retainerCity: int
     retainerID: int
@@ -80,9 +100,9 @@ class CurrentKeysTyped(TypedDict, total=False):
     buyerName: str
 
 
-class CurrentTyped(TypedDict, total=False):
-    """
-    -> Univertsalis API Current DC/World JSON Response. \n
+class CurrentDCWorlds(TypedDict, total=False):
+    """Univertsalis API Current DC/World JSON Response.
+
     `./universalis_data/data/universalis_api_current_dc.json`
     `./universalis_data/data/universalis_api_current_world.json`
     """
@@ -91,8 +111,8 @@ class CurrentTyped(TypedDict, total=False):
     worldID: int
     lastUploadTime: int
     dcName: str  # DC only
-    listings: Required[list[CurrentKeysTyped]]
-    recentHistory: Required[list[CurrentKeysTyped]]
+    listings: Required[list[CurrentKeys]]
+    recentHistory: Required[list[CurrentKeys]]
     currentAveragePrice: float | int
     currentAveragePriceNQ: float | int
     currentAveragePriceHQ: float | int
@@ -120,10 +140,10 @@ class CurrentTyped(TypedDict, total=False):
     hasData: bool
 
 
-class HistoryEntriesTyped(TypedDict, total=False):
-    """
-    Universalis API History related Typed Dict.\n
-    - Related to :class:`HistoryTyped.entries`.
+class HistoryEntries(TypedDict, total=False):
+    """Universalis API History.
+
+    Related to :class:`HistoryTyped.entries`.
     """
 
     hq: bool
@@ -136,9 +156,9 @@ class HistoryEntriesTyped(TypedDict, total=False):
     worldID: NotRequired[int]
 
 
-class HistoryTyped(TypedDict):
-    """
-    -> Universalis API History DC/World JSON Response. \n
+class History(TypedDict):
+    """Universalis API History DC/World JSON Response.
+
     `./local_data/api_examples/universalis_api_history_dc.json`
     `./local_data/api_examples/universalis_api_history_world.json`
     """
@@ -146,7 +166,7 @@ class HistoryTyped(TypedDict):
     itemID: int
     worldID: NotRequired[int]
     lastUploadTime: int
-    entries: list[HistoryEntriesTyped]
+    entries: list[HistoryEntries]
     dcName: NotRequired[str]
     stackSizeHistogram: dict[str, int]
     stackSizeHistogramNQ: dict[str, int]
@@ -157,11 +177,10 @@ class HistoryTyped(TypedDict):
     worldName: NotRequired[str]
 
 
-class MultiCurrentDataTyped(TypedDict):
-    """
-    MultiCurrentData is a representation of a bulk/multi item Universalis query.
-    - The key in items is the `item_id` query'd.
+class MultiCurrentData(TypedDict):
+    """MultiCurrentData is a representation of a bulk/multi item Universalis query.
 
+    The key in items is the `item_id` queried.
     """
 
-    items: dict[str, CurrentTyped]
+    items: dict[str, CurrentDCWorlds]
